f081a0ac89fa505a002d2e4c49a975f2c96fbc53
Md. Masuder Rahman <rahmansandwipe@gmail.com>
2024-06-09 14:26:22 +0600

    breeze install & env file modify

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('clien_id')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
+            $table->string('phone')->unique();
+            $table->enum('type', ['client','admin'])->default('client');
+            $table->enum('status', ['public','private'])->default('private');
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
